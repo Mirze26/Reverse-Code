109e672c7798892ea0dae04177b7ab6df6bf53b6
unknown <mirzaab@code.edu.az>
2022-12-13 02:52:51 +0400

    ReverseCode

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,160 @@
+﻿//verilmis n, m her ikisi cutdurse cemini tapun.
+
+//static int SumEvenNums (int n, int m)
+//{
+//    int sum = 0;
+
+//	if (n%2==0 && m%2==0)
+//	{
+//		sum = n + m;
+//	}
+//	return sum;
+
+
+
+
+//}
+//int num1 = 6;
+//int num2 = 8;
+//var result = SumEvenNums(num1, num2);
+//Console.WriteLine(result);
+
+
+
+// verilmis n ve m ededleri arasindaki tek ededlerin cemi.
+
+
+//static int SumOddNums (int n,int m)
+//{
+//    int sum = 0;
+//	for (int i = n; i < m; i++)
+//	{
+//		if (i%2==1)
+//		{
+//			sum += i;
+//		}
+
+//	}
+//	return sum;
+
+//}
+
+//int result = SumOddNums(3, 9);
+//Console.WriteLine(result);
+
+
+
+// verilmis arr-in icinnen cut ededlerin sayini tapun.
+
+
+//static int GetCountofEvenNumsFromArray (int[] nums)
+//{
+//    int count = 0;
+//	foreach (var item in nums)
+//	{
+//		if (item%2==0)
+//		{
+//			count++;
+//		}
+
+
+//	}
+//	return count;
+
+//}
+//int[] arr = { 2, 4, 5, 6, 8, 7, 9, 10 };
+//var result = GetCountofEvenNumsFromArray(arr);
+//Console.WriteLine(result);
+
+
+//sade metod tipi
+
+//static void P135()
+//{
+//    Console.WriteLine("CodeAcademy");
+//}
+// P135();
+
+
+
+// Metod OwerLoading
+
+
+//namespace Heap_Stack
+//{
+//    internal class Class1
+//    {
+//        public static void Test()
+//        {
+
+//            Console.WriteLine("Siemens");
+//        }
+
+//        public static void Test(int n)
+//        {
+//            Console.WriteLine(n);
+//        }
+
+
+//        public static string Test(int m, int t)
+//        {
+//            return "";
+//        }
+
+//        public static void Test(string n, int m)
+//        {
+//            Console.WriteLine(n + m);
+//        }
+
+//    }
+
+
+//}
+
+
+
+//int[] arr = { 2, 3, 4, 5, 6 };
+
+//Task(arr);
+//static void Task(int[] arr)
+//{
+//    Console.WriteLine(arr[0]);
+//}
+
+//string address = "BulBule";
+//if (address.EndsWith ("e") )
+//{
+//    Console.WriteLine("Duz diyirsen");
+//}
+//else
+//{
+//    Console.WriteLine("Yalan diyirsen");
+//
+
+
+
+// TersCode
+
+//static string Reverse(string Name)
+//{
+
+
+//    char[] arr = Name.ToCharArray();
+
+
+//    string reverse = string.Empty;
+
+
+//    for (int i = arr.Length - 1; i > -1; i--)
+//    {
+
+
+//        reverse += arr[i];
+//    }
+
+
+//    return reverse;
+//}
+
+//Console.WriteLine(Reverse("Code Academy"));
+
